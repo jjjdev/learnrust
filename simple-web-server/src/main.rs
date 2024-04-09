8f0c54aa747a74e4ec2c6c39883b28063447bcb6
J Jackson <jjjacksondev@gmail.com>
2024-04-08 20:50:09 -0400

    added respose handling

--- simple-web-server/src/main.rs
+++ simple-web-server/src/main.rs
@@ -3,9 +3,13 @@ use std::io::prelude::*;
 
 fn main() {
 
-    println!("Starting server!");
+    let address = "127.0.0.1:4221";
+    
+    println!("Starting server at {}!", address);
 
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind(address).unwrap();
+
+    println!("Now listening for traffic.");
 
     for stream in listener.incoming() {
         match stream {
@@ -31,9 +35,22 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", request);
 
     // --------- Handle response ------------
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let response = build_response(request.to_string());
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 
+}
+
+fn build_response(request: String) -> String {
+
+    let lines: Vec<&str> = request.lines().collect();
+    if !lines.is_empty(){
+        let req_target: Vec<&str> = lines[0].split_whitespace().collect();
+        if req_target.len() > 1 && req_target[1] == "/" {
+            return "HTTP/1.1 200 OK\r\n\r\n".to_string();
+        }
+    }
+
+    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
 }
